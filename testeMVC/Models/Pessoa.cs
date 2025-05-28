4f95cf9cb656c5954a438e445a013ce7900f7558
HneiroceHipo <henricorolim@gmail.com>
2025-05-28 10:50:02 -0300

    Atuakizado

--- testeMVC/Models/Pessoa.cs
+++ testeMVC/Models/Pessoa.cs
@@ -1,7 +1,11 @@
-﻿namespace testeMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testeMVC.Models
 {
     public class Pessoa
     {
+        [Display(Name = "Codigo do Cliente")]
+        [Display(Name = "Nome do Cliente")]
         public int? PessoaId { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
