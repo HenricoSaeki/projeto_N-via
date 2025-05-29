998c1c8d785a50f5d1de2b3bbb7f59f086da4939
HenricoeHipo <henricorolim@gmail.com>
2025-05-29 08:17:41 -0300

    Agora tá tudo certo

--- testeMVC/Models/Pessoa.cs
+++ testeMVC/Models/Pessoa.cs
@@ -5,9 +5,10 @@ namespace testeMVC.Models
     public class Pessoa
     {
         [Display(Name = "Codigo do Cliente")]
-        [Display(Name = "Nome do Cliente")]
         public int? PessoaId { get; set; }
+        [Display(Name = "Nome do Cliente")]
         public string? Nome { get; set; }
+        [Display(Name = "Email do Cliente")]
         public string? Email { get; set; }
     }
 }
